9a4ff0ec202e1b30ccdbded70ceffcbd4f1bc6f3
thedemon12 <kartikbhalla12@gmail.com>
2020-06-22 02:00:36 +0530

    Updates

---
+++ precache-manifest.dd9e66eec4c9a38430a79039e9fb6c49.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c58e3c622b62419af94f722f0afd8ef",
+    "url": "/LenDen/index.html"
+  },
+  {
+    "revision": "68330c28afe956d888ab",
+    "url": "/LenDen/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "bcec4bf6112e171185e2",
+    "url": "/LenDen/static/css/main.5404fe1d.chunk.css"
+  },
+  {
+    "revision": "68330c28afe956d888ab",
+    "url": "/LenDen/static/js/2.3c665dcc.chunk.js"
+  },
+  {
+    "revision": "fd7b5539dde28464d9cf26d9b1faefb8",
+    "url": "/LenDen/static/js/2.3c665dcc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcec4bf6112e171185e2",
+    "url": "/LenDen/static/js/main.4b1eddc0.chunk.js"
+  },
+  {
+    "revision": "e2aeb7beb3251c8ce72d",
+    "url": "/LenDen/static/js/runtime-main.239590be.js"
+  }
+]);
