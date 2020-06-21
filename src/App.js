cec0b315863b9d76180aa3eb04373faa9ae57f10
Kartik <kartikbhalla12@gmail.com>
2020-06-22 01:34:38 +0530

    comingSoon: initialize page
    Signed-off-by: Kartik <kartikbhalla12@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,26 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Image } from 'react-bootstrap';
+import moment from 'moment';
+import 'moment-countdown';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	state = {
+		date: {
+			days: 0,
+			hours: 0,
+			minutes: 0,
+			seconds: 0,
+		},
+	};
+
+	componentDidMount = () => {
+		this.myInterval = setInterval(() => {
+			const deadline = '2020-07-01';
+
+			const timeLeft = moment(deadline).countdown();
+			this.setState({
+				date: {
+					days: timeLeft.days,
+					hours: timeLeft.hours,
+					minutes: timeLeft.minutes,
+					seconds: timeLeft.seconds,
+				},
+			});
+		}, 1000);
+	};
+
+	render() {
+		const { days, minutes, hours, seconds } = this.state.date;
+		return (
+			<div className='mainBox'>
+				<div className='subBox'>
+					<Image
+						src='/demoLogo.png'
+						style={{ maxWidth: '300px', width: '50vw' }}
+					/>
+					<h1 className='companyName'>LENDEN</h1>
+					<p className='comingSoon'>Our Website is Coming Soon !</p>
+					<div
+						className='timerBox'
+						style={{
+							display: 'flex',
+						}}>
+						<div className='timerElement' style={{ flexBasis: '25%' }}>
+							<div>{days}</div>
+							<div>Days</div>
+						</div>
+						<div className='timerElement' style={{ flexBasis: '25%' }}>
+							<div>{hours}</div>
+							<div>Hours</div>
+						</div>
+						<div className='timerElement' style={{ flexBasis: '25%' }}>
+							<div>{minutes}</div>
+							<div>Minutes</div>
+						</div>
+						<div className='timerElement' style={{ flexBasis: '25%' }}>
+							<div>{seconds}</div>
+							<div>Seconds</div>
+						</div>
+					</div>
+				</div>
+				<p>Subscribe to our Newsletter ?</p>
+			</div>
+		);
+	}
 }
 
 export default App;
