6e3eb10ba93dc4529865c7e4c11546fca8977159
thedemon12 <kartikbhalla12@gmail.com>
2020-06-23 01:17:14 +0530

    app: finalize page
    Signed-off-by: thedemon12 <kartikbhalla12@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Image, Form, Button } from 'react-bootstrap';
 import moment from 'moment';
 import 'moment-countdown';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
 import './App.css';
+import CommonForm from './components/common/commonForm';
+import Joi from 'joi-browser';
 
-class App extends Component {
+class App extends CommonForm {
 	state = {
+		data: {},
+		error: '',
+		success: '',
+		loading: false,
 		date: {
 			days: 0,
 			hours: 0,
 			minutes: 0,
 			seconds: 0,
 		},
+	};
+
+	schema = {
+		email: Joi.string().email().required().label('Email'),
 	};
 
 	componentDidMount = () => {
@@ -32,9 +42,20 @@ class App extends Component {
 		}, 1000);
 	};
 
-	handleSubmit = (e) => {
-		e.preventDefault();
-		console.log('submitted');
+	doSubmit = async () => {
+		const { data } = this.state;
+		this.setState({ loading: true });
+
+		try {
+			setTimeout(() => {
+				const success = 'Subscribed Successfully!';
+				this.setState({ loading: false, success });
+			}, 2000);
+		} catch (ex) {
+			const error = 'Something Failed';
+			this.setState({ error });
+			this.setState({ loading: false });
+		}
 	};
 
 	render() {
@@ -43,13 +64,18 @@ class App extends Component {
 			<div className='mainBox'>
 				<div className='subBox'>
 					<Image
-						src={`${process.env.PUBLIC_URL}/demoLogo.png`}
-						style={{ maxWidth: '300px', width: '50vw' }}
+						src={`${process.env.PUBLIC_URL}/logo.png`}
+						style={{
+							maxWidth: '260px',
+							width: '40vw',
+							borderRadius: '100%',
+							// border: '2px solid #424242',
+						}}
 					/>
 					<h1 className='companyName'>Hola Amigos, </h1>
 					<p
 						className='comingSoon'
-						style={{ marginBottom: '6vh', marginTop: '1vh' }}>
+						style={{ marginBottom: '4vh', marginTop: '1vh' }}>
 						We hope you are well and safe during these tough times.
 					</p>
 					<p className='comingSoon'>
@@ -63,48 +89,99 @@ class App extends Component {
 						className='timerBox'
 						style={{
 							display: 'flex',
+							// border: '1px solid #FDBA49',
+							padding: '10px',
+							alignItems: 'flex-end',
 						}}>
-						<div className='timerElement' style={{ flexBasis: '25%' }}>
+						<div
+							className='timerElement'
+							style={{ flexBasis: '25%', position: 'relative' }}>
+							<Image
+								src={`${process.env.PUBLIC_URL}/phone.png`}
+								style={{
+									maxHeight: '13vw',
+									maxWidth: '70%',
+								}}
+							/>
 							<div>{days}</div>
-							<div>Days</div>
+							<div className='timerElementName'>Days</div>
 						</div>
-						<div className='timerElement' style={{ flexBasis: '25%' }}>
+						<div
+							className='timerElement'
+							style={{ flexBasis: '25%', position: 'relative' }}>
+							<Image
+								src={`${process.env.PUBLIC_URL}/book.png`}
+								style={{
+									maxHeight: '15vw',
+									maxWidth: '60%',
+								}}
+							/>
 							<div>{hours}</div>
-							<div>Hours</div>
+							<div className='timerElementName'>Hours</div>
 						</div>
-						<div className='timerElement' style={{ flexBasis: '25%' }}>
+						<div
+							className='timerElement'
+							style={{ flexBasis: '25%', position: 'relative' }}>
+							<Image
+								src={`${process.env.PUBLIC_URL}/gaming.png`}
+								style={{
+									maxHeight: '15vw',
+									maxWidth: '70%',
+								}}
+							/>
 							<div>{minutes}</div>
-							<div>Minutes</div>
+							<div className='timerElementName'>Minutes</div>
 						</div>
-						<div className='timerElement' style={{ flexBasis: '25%' }}>
+						<div
+							className='timerElement'
+							style={{ flexBasis: '25%', position: 'relative' }}>
+							<Image
+								src={`${process.env.PUBLIC_URL}/headphones.png`}
+								style={{
+									maxHeight: '15vw',
+									maxWidth: '70%',
+								}}
+							/>
 							<div>{seconds}</div>
-							<div>Seconds</div>
+							<div className='timerElementName'>Seconds</div>
 						</div>
 					</div>
 				</div>
-				<div>
+				<div
+					style={{
+						border: '1px solid #FDBA49',
+						marginTop: '6vh',
+						width: '80vw',
+						maxWidth: '400px',
+						padding: '15px 25px',
+						margin: '5vh auto ',
+					}}>
 					<FontAwesomeIcon
 						icon={faPaperPlane}
 						style={{
-							color: '#fff',
+							color: '#FDBA49',
 							fontSize: '35px',
-							marginTop: '6vh',
+							marginTop: '1vh',
 							marginBottom: '2vh',
 						}}
 					/>
 					<p className='newsletter'>Do not miss any update from us.</p>
-					<p className='newsletter'>Subscribe to our newsletter</p>
+					<p className='newsletter'>Subscribe to our Newsletter</p>
 					<Form
 						style={{ maxWidth: 300, margin: 'auto' }}
 						noValidate
 						onSubmit={this.handleSubmit}>
-						<Form.Group controlId='formBasicEmail'>
-							<Form.Control type='email' placeholder='Email Address' />
-						</Form.Group>
+						{this.renderInput('email', 'Email Address')}
+						{this.renderAlert()}
+						{this.renderSuccessAlert()}
+						{this.renderLoader()}
 						<Button
 							variant='primary'
 							type='submit'
-							style={{ backgroundColor: '#ef5350', border: 'none' }}>
+							style={{
+								backgroundColor: '#FDBA49',
+								border: 'none',
+							}}>
 							Subscribe
 						</Button>
 					</Form>
---
+++ src/components/common/commonForm.jsx
@@ -0,0 +1,97 @@
+import React, { Component } from 'react';
+import Joi from 'joi-browser';
+import { Form, Alert } from 'react-bootstrap';
+import BarLoader from 'react-spinners/BarLoader';
+
+class CommonForm extends Component {
+	state = {
+		data: {},
+		error: '',
+		success: '',
+	};
+	handleChange = ({ currentTarget: input }) => {
+		const data = { ...this.state.data };
+		data[input.name] = input.value;
+
+		this.setState({ data });
+	};
+
+	validate = () => {
+		const { error } = Joi.validate(this.state.data, this.schema);
+		if (error) return error.details[0].message;
+		return '';
+	};
+
+	handleSubmit = (e) => {
+		e.preventDefault();
+		this.setState({
+			success: '',
+			error: '',
+		});
+		const error = this.validate();
+		this.setState({ error });
+		if (error) return;
+
+		this.doSubmit();
+	};
+
+	renderInput(name, placeholder) {
+		const { data } = this.state;
+		return (
+			<Form.Group controlId={name}>
+				<Form.Control
+					className='input'
+					name={name}
+					type={name}
+					placeholder={placeholder}
+					value={data[name]}
+					onChange={this.handleChange}
+				/>
+			</Form.Group>
+		);
+	}
+	renderAlert = () => {
+		const { error } = this.state;
+		return (
+			error && (
+				<Alert id='error-alert' variant='danger'>
+					{error}
+				</Alert>
+			)
+		);
+	};
+
+	renderSuccessAlert = () => {
+		const { success } = this.state;
+		return (
+			success && (
+				<Alert id='error-success' variant='primary'>
+					{success}
+				</Alert>
+			)
+		);
+	};
+
+	renderLoader = () => {
+		return (
+			<BarLoader
+				css={{
+					display: 'block',
+					margin: '3vh auto 3vh auto',
+				}}
+				size={50}
+				color={'#FDBA49'}
+				loading={this.state.loading}
+			/>
+		);
+	};
+
+	resetForm = () => {
+		const data = {
+			email: '',
+		};
+		this.setState({ data });
+	};
+}
+
+export default CommonForm;
